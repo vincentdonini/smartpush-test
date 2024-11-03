eaa6b2c27a7b3143d42ae1b37e8b2423cd7496f8
vincentdonini <vincent.donini@tradedoubler.com>
2024-11-03 14:27:01 +0100

    Add NelmioCorsBundle symfony

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
